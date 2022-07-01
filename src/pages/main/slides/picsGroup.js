6ce0eac9db2a3942379c371bb934964a1d116c79
Erfan <erfan.nemati@digikala.com>
2022-07-01 17:43:14 +0430

    fixing issues

--- src/pages/main/slides/picsGroup.js
+++ src/pages/main/slides/picsGroup.js
@@ -3,24 +3,16 @@ import PicsGroup2 from "./picsGroup2";
 import PicsGroup3 from "./picsGroup3";
 import PicsGroup1 from "./picsGroup1";
 
-
 function PicsGroup() {
-    return (
-        <>
-            <div>
-                <PicsGroup1 />
-            </div>
-            <div className="hidden">
-                <PicsGroup2 />
-            </div>
-            <div className="hidden">
-                <PicsGroup3 />
-            </div>
-        </>
+  return (
+    <div className="d-flex w-100 overflow-auto">
+      <PicsGroup1 className="flex-grow-1 flex-shrink-0" />
 
+      <PicsGroup2 className="flex-grow-1 flex-shrink-0" />
 
-
-    )
+      <PicsGroup3 className="flex-grow-1 flex-shrink-0" />
+    </div>
+  );
 }
 
-export default PicsGroup
+export default PicsGroup;
--- src/pages/main/slides/slideShow.js
+++ src/pages/main/slides/slideShow.js
@@ -1,27 +1,27 @@
-import React, { useRef, useState } from 'react';
-import SwiperCore, { Virtual, Navigation, Pagination } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useRef, useState } from "react";
+import SwiperCore, { Virtual, Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { isMobile } from "react-device-detect";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 // install Virtual module
 SwiperCore.use([Virtual, Navigation, Pagination]);
 
 export default function SlideShow() {
-    const [resize, setResize] = useState(window.innerWidth);
-    const [swiperRef, setSwiperRef] = useState(null);
-    const appendNumber = useRef(10);
-    const prependNumber = useRef(1);
-    // Create array with 10 slides
-    const [slides, setSlides] = useState(
-        Array.from({ length: 10 }).map((_, index) => `Slide ${index + 1}`)
-    );
+  const [resize, setResize] = useState(window.innerWidth);
+  const [swiperRef, setSwiperRef] = useState(null);
+  const appendNumber = useRef(10);
+  const prependNumber = useRef(1);
+  // Create array with 10 slides
+  const [slides, setSlides] = useState(
+    Array.from({ length: 10 }).map((_, index) => `Slide ${index + 1}`)
+  );
 
-
-    /*  const prepend = () => {
+  /*  const prepend = () => {
           setSlides([
               `Slide ${prependNumber.current - 2}`,
               `Slide ${prependNumber.current - 1}`,
@@ -39,27 +39,24 @@ export default function SlideShow() {
           swiperRef.slideTo(index - 1, 0);
       };*/
 
-    return (
-        <>
-            <Swiper
-                onSwiper={setSwiperRef}
-                slidesPerView={3}
-                centeredSlides={true}
-                spaceBetween={10}
-                pagination={{
-                    type: 'fraction',
-                }}
-                navigation={true}
-                virtual
+  return (
+    <>
+      <Swiper
+        slidesPerView={isMobile ? 2 : 5}
+        spaceBetween={10}
+        navigation={true}
+      >
+        {slides.map((slideContent, index) => (
+          <SwiperSlide key={slideContent} virtualIndex={index}>
+            <div
+              className="d-flex align-items-center justify-content-center"
+              style={{ width: "100px", height: "100px" }}
             >
-                {slides.map((slideContent, index) => (
-                    <SwiperSlide key={slideContent} virtualIndex={index}>
-                        {slideContent}
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-
-
-        </>
-    );
+              {slideContent}
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
 }
